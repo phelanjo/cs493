ea2851d867f2b5f16335d69bee1b4bcde566817d
Johnny Phelan <phelanjo@oregonstate.edu>
2020-01-09 08:58:31 -0800

    Add sign up form, button, functionality.

--- cs493-notes/src/App.js
+++ cs493-notes/src/App.js
@@ -6,7 +6,7 @@ function App() {
     let googleLogin = document.getElementById('googleLogin');
     let signIn = document.getElementById('login')
     let signOut = document.getElementById('signOut')
-    
+    let signup = document.getElementById('signup')
 
     googleLogin.addEventListener('click', () => {
       var googleProvider = new firebase.auth.GoogleAuthProvider()
@@ -41,10 +41,24 @@ function App() {
           console.log(err)
         })
     }, false)
+
+    signup.addEventListener('click', (e) => {
+      let signUpEmail = document.getElementById('signUpEmail').value
+      let signUpPassword = document.getElementById('signUpPassword').value
+
+      firebase.auth().createUserWithEmailAndPassword(signUpEmail, signUpPassword)
+        .then(user => {
+          console.log(user)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    })
   }
 
   return (
     <div className="container center App">
+
       <div>
         <form id="signIn">
           <label htmlFor="email">Email</label>
@@ -54,14 +68,31 @@ function App() {
         </form>
         <button className="btn blue" id="login">Sign In</button>
       </div>
+
       <br />
+
       <div>
         <button className="btn red" id="googleLogin">Google Sign In</button>
       </div>
+
       <br />
+
       <div>
         <button className="btn red" id="signOut">Sign Out</button>
       </div>
+
+      <br /> <br />
+
+      <div>
+        <form id="signUp">
+          <label htmlFor="email">Sign Up Email</label>
+          <input type="email" id="signUpEmail"/>
+          <label htmlFor="password">Sign Up Password</label>
+          <input type="password" id="signUpPassword"/>
+        </form>
+        <button className="btn green" id="signup">Sign Up</button>
+      </div>
+      
     </div>
   );
 }
