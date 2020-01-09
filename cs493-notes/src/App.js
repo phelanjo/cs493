2f6882ccfec11343fcda0ee7070d2885fce5b56c
Johnny Phelan <phelanjo@oregonstate.edu>
2020-01-09 08:43:07 -0800

    Add functonality for Google login button.

--- cs493-notes/src/App.js
+++ cs493-notes/src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import firebase from './config/firebaseConfig'
 
 function App() {
+  window.onload = () => {
+    let googleLogin = document.getElementById('googleLogin');
+
+    googleLogin.addEventListener('click', () => {
+      var googleProvider = new firebase.auth.GoogleAuthProvider()
+  
+      firebase.auth().signInWithPopup(googleProvider).then((result) => {
+        var user = result.user;
+        console.log(user)
+      }).catch((err) => {
+        console.log(err)
+      })
+    })
+  }
+
   return (
     <div className="container center App">
         <div>
