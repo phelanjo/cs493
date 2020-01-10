d14838d5bf57224685703d1b1bbb346c74275a14
Johnny Phelan <phelanjo@oregonstate.edu>
2020-01-09 20:28:37 -0800

    Export dashboard

--- cs493-notes/src/components/Dashboard.js
+++ cs493-notes/src/components/Dashboard.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import firebase from '../config/firebaseConfig'
 
 import SignOutButton from './SignOutButton'
 
 class Dashboard extends Component {
   render() {
     return (
-      <SignOutButton />
+      <div className="center">
+        <SignOutButton />
+      </div>
     )
   }
 }
+
+export default Dashboard
