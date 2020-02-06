0b7220cb1596d32c2111d0f526084dd81af21db4
Johnny Phelan <phelanjo@oregonstate.edu>
2020-02-06 09:21:35 -0800

    Implement assume role using aws-sdk

---
+++ aws-sdk.nosync/assumeRole.js
@@ -0,0 +1,26 @@
+var AWS = require('aws-sdk')
+
+var sts = new AWS.STS()
+
+const argv = require('yargs')
+  .option('roleArn', {
+    alias: 'rA',
+    describe: 'enter the role ARN',
+    demandOption: true
+  })
+  .option('roleSessionName', {
+    alias: 'rSN',
+    describe: 'enter a role session name',
+    demandOption: true
+  })
+  .help().argv
+
+var params = {
+  RoleArn: argv.roleArn,
+  RoleSessionName: argv.roleSessionName
+}
+
+sts.assumeRole(params, function(err, data) {
+  if (err) console.log(err, err.stack)
+  else console.log(data)
+})
