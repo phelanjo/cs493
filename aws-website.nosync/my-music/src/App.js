cab4249e67cf1d4892c1e616fa5e393a01d965d5
Johnny Phelan <phelanjo@oregonstate.edu>
2020-02-27 12:47:04 -0800

    Update website to use updated api and Auth feature.

--- aws-website.nosync/my-music/src/App.js
+++ aws-website.nosync/my-music/src/App.js
@@ -1,128 +1,296 @@
 import React, { Component } from 'react';
 
 import Navbar from './components/Navbar';
+import Auth from './components/Auth';
+
+const API_ADDRESS = 'http://100.26.183.29:8080';
 
 class App extends Component {
-  state = {
-    isLoaded: false,
-    song: null,
-    songs: null,
-    songUrl: null,
-    selectedSongIdx: null
-  };
+  _isMounted = false;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+      isLoaded: false,
+
+      genres: null,
+      selectedGenre: null,
+
+      artists: null,
+      selectedArtist: null,
+
+      albums: null,
+      selectedAlbum: null,
+
+      songs: null,
+      selectedSong: null,
+      selectedSongIdx: null,
+      songUrl: null
+    };
+  }
 
   componentDidMount() {
-    fetch('http://3.83.243.250:8080')
+    fetch(`${API_ADDRESS}/genres`)
       .then(res => {
         return res.json();
       })
       .then(data => {
-        this.setState({ songs: data, isLoaded: true });
+        this.setState({ genres: data });
       })
       .catch(err => {
         console.log(err);
       });
+  }
+
+  getAuthData = authData => {
+    this.setState({
+      isLoggedIn: authData.isLoggedIn,
+      isLoaded: true,
+      selectedGenre: null,
+      selectedArtist: null,
+      selectedAlbum: null,
+      selectedSong: null
+    });
+  };
+
+  renderGenres() {
+    const { genres } = this.state;
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Genres</th>
+          </tr>
+        </thead>
+        <tbody>
+          {genres.map((genre, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <button
+                    className={'btn teal darken-3'}
+                    onClick={() => {
+                      this.selectGenre(index);
+                      this.setState({
+                        selectedArtist: null,
+                        selectedAlbum: null,
+                        selectedSong: null
+                      });
+                    }}
+                  >
+                    {genre}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+
+  renderArtists() {
+    const { artists } = this.state;
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Artists</th>
+          </tr>
+        </thead>
+        <tbody>
+          {artists.map((artist, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <button
+                    className={'btn teal darken-3'}
+                    onClick={() => {
+                      this.selectArtist(index);
+                      this.setState({
+                        selectedAlbum: null,
+                        selectedSong: null
+                      });
+                    }}
+                  >
+                    {artist}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+
+  renderAlbums() {
+    const { albums } = this.state;
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Albums</th>
+          </tr>
+        </thead>
+        <tbody>
+          {albums.map((album, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <button
+                    className={'btn teal darken-3'}
+                    onClick={() => {
+                      this.selectAlbum(index);
+                      this.setState({
+                        selectedSong: null
+                      });
+                    }}
+                  >
+                    {album}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
   }
 
   renderSongs() {
     const { songs } = this.state;
 
     return (
-      <tbody>
-        {songs.map((s, index) => {
-          return (
-            <tr key={index}>
-              <td>{songs[index].artist}</td>
-              <td>{songs[index].album}</td>
-              <td>
-                {songs[index].song
-                  .split('.')
-                  .slice(0, -1)
-                  .join('.')}
-              </td>
-              <td>
-                <button
-                  className="btn-floating btn-small teal darken-3"
-                  onClick={() => this.selectSong(index)}
-                >
-                  <i className="small material-icons">play_arrow</i>
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
+      <table>
+        <thead>
+          <tr>
+            <th>Songs</th>
+          </tr>
+        </thead>
+        <tbody>
+          {songs.map((song, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <button
+                    className={'btn teal darken-3'}
+                    onClick={() => this.selectSong(index)}
+                  >
+                    {song}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     );
   }
 
-  selectSong(index) {
-    const { songs } = this.state;
-    const selectedSong = songs[index];
-    var songKey = `${selectedSong.artist}/${selectedSong.album}/${selectedSong.song}`;
+  playSong() {
+    const { songUrl } = this.state;
 
-    const params = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        key: songKey
-      })
-    };
+    return (
+      <audio controls autoPlay ref="audio" id="audio">
+        <source src={songUrl} type="audio/mp4" />
+      </audio>
+    );
+  }
 
-    fetch('http://3.83.243.250:8080', params)
+  selectGenre(index) {
+    const { genres } = this.state;
+    const selectedGenre = genres[index];
+
+    fetch(`${API_ADDRESS}/artists/for/genre?genre=${selectedGenre}`)
       .then(res => res.json())
-      .then(res => {
-        // help from -- https://stackoverflow.com/a/43578104/9487966
-        this.setState({ songUrl: res.url, selectedSongIdx: index }, () => {
-          this.refs.audio.pause();
-          this.refs.audio.load();
-          this.refs.audio.play();
-        });
+      .then(data => {
+        this.setState({ artists: data, selectedGenre });
       })
       .catch(err => {
         console.log(err);
       });
   }
 
-  playSong() {
-    const { songUrl, songs, selectedSongIdx } = this.state;
-    return (
-      <div className="center">
-        <p>
-          {songs[selectedSongIdx].artist +
-            ' - ' +
-            songs[selectedSongIdx].song
-              .split('.')
-              .slice(0, -1)
-              .join('.')}
-        </p>
-        <audio controls autoPlay ref="audio">
-          <source src={songUrl} type="audio/mp4" />
-        </audio>
-      </div>
-    );
+  selectArtist(index) {
+    const { artists } = this.state;
+    const selectedArtist = artists[index];
+
+    fetch(`${API_ADDRESS}/albums/for/artist?artist=${selectedArtist}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ albums: data, selectedArtist });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  selectAlbum(index) {
+    const { albums } = this.state;
+    const selectedAlbum = albums[index];
+
+    fetch(`${API_ADDRESS}/songs/for/album?album=${selectedAlbum}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ songs: data, selectedAlbum });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  selectSong(index) {
+    const { songs } = this.state;
+    const selectedSong = songs[index];
+
+    fetch(`${API_ADDRESS}/song?song=${selectedSong}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState(
+          { songUrl: data.url, selectedSong, selectedSongIdx: index },
+          () => {
+            this.refs.audio.pause();
+            this.refs.audio.load();
+            this.refs.audio.play();
+          }
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
-    const { songUrl } = this.state;
+    const {
+      selectedGenre,
+      selectedArtist,
+      selectedAlbum,
+      selectedSong
+    } = this.state;
+
     return (
       <div>
         <Navbar />
-        <div className="container">
-          {songUrl ? this.playSong() : null}
-          <table>
-            <thead>
-              <tr>
-                <th>Artist</th>
-                <th>Album</th>
-                <th>Song</th>
-                <th></th>
-              </tr>
-            </thead>
-            {this.state.isLoaded ? this.renderSongs() : null}
-          </table>
+        <Auth getAuthData={this.getAuthData} />
+        <div className="container center">
+          {selectedSong ? (
+            <div className="musicPlayer teal darken-3">
+              <h5>
+                {selectedArtist} - {selectedSong}
+              </h5>
+              {this.playSong()}
+            </div>
+          ) : null}
+          {this.state.isLoggedIn ? this.renderGenres() : null}
+          {selectedGenre ? this.renderArtists() : null}
+          {selectedArtist ? this.renderAlbums() : null}
+          {selectedAlbum ? this.renderSongs() : null}
         </div>
       </div>
     );
